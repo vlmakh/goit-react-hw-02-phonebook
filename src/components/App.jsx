f3fd6740f9857e347bd59439a3bd0038ab55e7c5
vlmakh <vlmakh@gmail.com>
2022-10-20 13:25:21 +0300

    add step2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,26 @@
 import { Component } from 'react';
 import css from './App.module.css';
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
 
 class App extends Component {
   state = {
     contacts: [
-      { id: nanoid(4), name: 'Arnold Schwarzenegger' },
-      { id: nanoid(4), name: 'Sylvester Stallone' },
-      { id: nanoid(4), name: 'Bruce Willis' },
-      { id: nanoid(4), name: 'Jason Statham' },
+      { id: nanoid(4), name: 'Arnold Schwarzenegger', number: '555-8801' },
+      { id: nanoid(4), name: 'Sylvester Stallone', number: '555-8802' },
+      { id: nanoid(4), name: 'Bruce Willis', number: '555-8803' },
+      { id: nanoid(4), name: 'Jason Statham', number: '555-8804' },
     ],
     name: '',
+    number: '',
   };
 
   render() {
     return (
       <>
         <form className={css.form}>
-          <h2>Phonebook</h2>
+          <h2 className={css.sectionTitle}>Phonebook</h2>
           <label className={css.formField}>
-            Имя:
+            <span className={css.formField__name}>Name</span>
             <input
               type="text"
               name="name"
@@ -29,15 +30,31 @@ class App extends Component {
             />
           </label>
 
+          <label className={css.formField}>
+            <span className={css.formField__name}>Number</span>
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+
           <button type="button" className={css.addButton}>
             Add contact
           </button>
         </form>
 
-        <div className={css.contactList}>
-          <h2>Contact list</h2>
-          <ul>
-            {this.state.contacts.map(contact => <li key={contact.id}>{contact.name}</li>)}
+        <div className={css.contactTable}>
+          <h2 className={css.sectionTitle}>Contact list</h2>
+          <ul className={css.contactList}>
+            {this.state.contacts.map(contact => (
+              <li key={contact.id} className={css.contactItem}>
+                <span className={css.contactName}>{contact.name}</span>{' '}
+                <span>{contact.number}</span>
+              </li>
+            ))}
           </ul>
         </div>
       </>
