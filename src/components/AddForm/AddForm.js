92aa628175eda6fb49d1c398cfb5ee764f94e232
vlmakh <vlmakh@gmail.com>
2022-10-22 14:08:47 +0300

    installed yup

--- src/components/AddForm/AddForm.js
+++ src/components/AddForm/AddForm.js
@@ -4,6 +4,12 @@ import { HiPhone, HiUserAdd } from 'react-icons/hi';
 import { MdOutlineDataSaverOn } from 'react-icons/md';
 import { Box } from 'components/Box/Box';
 import { Formik, Form, Field } from 'formik';
+// import * as yup from 'yup';
+
+// let schema = yup.object().shape({
+//   name: yup.string().required(),
+//   number: yup.number().required(),
+// });
 
 export function AddForm({ onSubmit }) {
   const addContact = (values, { resetForm }) => {
@@ -18,6 +24,7 @@ export function AddForm({ onSubmit }) {
         name: '',
         number: '',
       }}
+      // validationSchema={schema}
     >
       <Form className={css.form}>
         <Box display="flex" justifyContent="space-between" alignItems="center">
@@ -29,10 +36,11 @@ export function AddForm({ onSubmit }) {
                 type="text"
                 name="name"
                 placeholder="Add new contact"
-                // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                // title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                 required
               />
+              {/* <ErrorMessage name="name" /> */}
             </Box>
 
             <Box display="flex" alignItems="center">
@@ -41,10 +49,11 @@ export function AddForm({ onSubmit }) {
                 className={css.addInput}
                 type="tel"
                 name="number"
-                // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                // title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                 required
               />
+              {/* <ErrorMessage name="number" /> */}
             </Box>
           </Box>
 
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -42,7 +42,7 @@ class App extends Component {
 
   deleteContact = contactId => {
     this.setState(prevState => ({
-      contacts: prevState.contacts.filter(el => el.id !== contactId),
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
 
